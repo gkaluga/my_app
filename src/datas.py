57c61a294ccd67750ab71eed5eea2e09fd6c9cf8
Gennady <46938551+gkaluga@users.noreply.github.com>
2024-08-31 09:40:26 +0300

    Delete src/datas.py
    error

--- src/datas.py
+++
@@ -1,9 +0,0 @@
-print('hello world')
-print(type('hello world'), type(5), type(False))
-print(5<4 or 7<10)
-
-print(bool('56'))
-
-name = 'Pobject'
-print(''.join(reversed("hello world")))
-print("hello world"[::-1])
